ce04955c5ff9da7011722813d6334307e9a6a99e
Adrián Rodríguez-Bazaga <adrianrodriguezbazaga@gmail.com>
2017-12-02 23:09:55 +0100

    Movement abstract class

--- Metaheuristics/GRASP/Movement.cpp
+++ Metaheuristics/GRASP/Movement.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "NurseSchedulingSolution.cpp"
 using namespace std;
 
 class Movement {
+    virtual NurseSchedulingSolution perform(const NurseSchedulingSolution&, int, int) = 0;
 };
