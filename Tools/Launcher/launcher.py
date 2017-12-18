0f6527fb0adac7de22dd60d08fdf40bbc68b27e4
surfuga <toymachin@gmail.com>
2017-12-18 23:52:53 +0100

    launcher.py to run oplrun, generator02.py to create some 24h instances between 400s and 1220s

--- Tools/Launcher/launcher.py
+++ Tools/Launcher/launcher.py
@@ -1,3 +1,11 @@
+import json
+import sys
+import os
+import traceback
+import shutil
+import re
+from subprocess import call,check_call,Popen, PIPE
+from pathlib import Path, PurePath
 
 
 """
@@ -10,3 +18,101 @@
         4- executes in shell the command "oplrun -v <name>.mod" with the env var LD_LIBRARY_PATH=/opt/ibm/ILOG/CPLEX_Studio126/opl/bin/x86-64_linux
 
 """
+mod_header_template = os.path.abspath('../Instance_Generator/Complex Generator/Test-header2.template')
+mod_footer_template = os.path.abspath('../Instance_Generator/Complex Generator/Test-footer.template')
+
+
+def shellexec(command, cwd="."):
+
+    my_env = os.environ.copy()
+    if my_env["PATH"].find("/usr/sbin:/sbin:") < 0:
+        my_env["PATH"] = "/usr/sbin:/sbin:" + my_env["PATH"]
+
+    my_env["LD_LIBRARY_PATH"] = "/opt/ibm/ILOG/CPLEX_Studio126/opl/bin/x86-64_linux"
+
+    p = Popen(
+        [command],
+        cwd=cwd,
+        shell=True,
+        env=my_env,
+        stdout=PIPE,
+        stderr=PIPE)
+
+    (out, err) = p.communicate()
+
+    return (out, err)
+
+
+def prepareModFile(instancepath, tilim=1800.0,  path=Path()):
+    print("preparing mod file for :" + instancepath)
+
+    filename = '../../Tools/Launcher/temp.mod'
+    filename = PurePath(path, filename)
+
+    modelfile = '../../ILP/model01.mod'
+    modelfile = os.path.abspath(modelfile)
+
+    logfile = os.path.splitext(os.path.basename(instancepath))[0] + '.json'
+    logname = '../../Results/Pending/' + logfile
+
+    with open(filename, 'w') as f:
+
+        with open(mod_header_template, 'r') as h:
+            for line in h:
+                if "model01-hfree.mod" in line:
+                    line = line.replace("model01-hfree.mod", str(modelfile))
+
+                if line.startswith("var logname = \""):
+
+                    line = "var logname = \"" + logname + "\";\n"
+
+                f.write(line)
+
+        f.write(
+            'myTest(def, cplex,"' +
+            instancepath +
+            '", logname, ' +
+            str(tilim) +
+            ',  "SUCCESS", 1 );\n')
+
+        with open(mod_footer_template,'r') as h:
+            for line in h:
+                f.write(line)
+
+    return str(filename)
+
+
+def solveInstanceWithILP(modfile):
+    print("solving instance " + modfile)
+    output, error = shellexec("oplrun -v " + modfile)
+    print("output = ")
+    print(output)
+
+
+
+instances_folder = "../../Instances/Pending/"
+
+if not os.path.exists(instances_folder):
+    print("check folder paths!")
+    exit()
+
+os.chdir(instances_folder)
+for root, dirs, files in os.walk("."):
+    all_ok = True
+    for instance in files:
+        try:
+            instancepath = os.path.join(root, instance)
+            print("processing instance " + instance)
+
+            mod_path = prepareModFile(instancepath)
+
+            solveInstanceWithILP(mod_path)            
+
+        except Exception:
+            all_ok = False
+            print("Exception in user code:")
+            print("-"*60)
+            traceback.print_exc(file=sys.stdout)
+            print("-"*60)
+    
+            
---
+++ Tools/Launcher/temp.mod
@@ -0,0 +1,119 @@
+  /*********************************************
+ * OPL 12.6.0.0 Model
+ * Author: Adrian Rodriguez Bazaga, Pau Rodriguez Esmerats
+ * Creation Date: 06/12/2017 at 14:53:13
+ *********************************************/
+
+ 
+ main {
+
+
+function cleanString(mystring){
+	
+	var returnstring = "";
+	var i = mystring.indexOf("\n");
+	var aux2 = mystring;
+	var j=0;
+	while (i != -1 && j<100){
+		var aux = aux2.substring(0,i);
+		aux2 = aux2.substring(i+1);
+		returnstring = returnstring + aux + "\\n";
+		i = aux2.indexOf("\n");
+		j = j +1;
+	}
+
+	return returnstring;
+}
+
+function myTest(def, cplex, filename, logname, tilim,  goal, objf,  showsol) {
+
+ var ofile = new IloOplOutputFile(logname, true);
+ ofile.write("{ \""+filename+"\" : ");
+ ofile.write("{");
+ //ofile.writeln("\"\" : \""++"\",");
+ 
+ var model = new IloOplModel(def,cplex);
+ 
+ var data = new IloOplDataSource(filename);
+ model.addDataSource(data);
+ model.generate();
+ //cplex.epgap=0.01;
+ // 
+ // 3600.0 1h
+ // 1800.0 30m
+ // 900.0 15m
+ // 300.0 5m
+ //  60.0 1m
+ cplex.TiLim=tilim;
+ //model.setParam(IloCplex.Param.tune.DetTimeLimit,10.0);
+ 
+ //try {
+
+  var solved = cplex.solve();  
+  ofile.writeln("\"expected_output\" : \""+goal+"\",");
+  ofile.writeln("\"computed_output\" : \""+solved+"\",");
+  ofile.writeln("\"desired_objfunc\" : \""+objf+"\",");
+	
+	if (solved){
+	 	
+	 	ofile.writeln("\"solver\" : \"ILP\",");
+	 	ofile.writeln("\"time\" : \""+cplex.getSolvedTime()+"\",");
+	 	ofile.writeln("\"int_vars\" : \""+cplex.getNintVars()+"\",");
+	 	//ofile.writeln("\"lower_bound\" : \""+cplex.getLb()+"\",");
+	 	ofile.writeln("\"Gap\" : \""+cplex.getMIPRelativeGap()+"\",");
+	 	ofile.writeln("\"ObjectiveFunction\" : \""+cplex.getObjValue()+"\",");
+	 	ofile.writeln("\"isMIP?\" : \""+cplex.isMIP()+"\",");
+	 	ofile.writeln("\"Data\" : \""+cleanString(model.printExternalData())+"\",");
+	 	ofile.writeln("\"Solution\" : \""+cleanString(model.printSolution())+"\"");
+ 	} else {
+		ofile.writeln("\"Data\" : \""+cleanString(model.printExternalData())+"\"");
+	
+ 	}	 	
+
+ 	
+	  
+// } catch (IloException e){
+//  	ofile.writeln("                --- Exception Occurred for : "+filename )
+//	ofile.writeln(model.printExternalData());	
+//	ofile.writeln(e.printStackTrace());
+// }
+
+ ofile.write("}},");
+
+ ofile.close();
+ data.end();
+ model.end();
+  return true;
+}
+
+
+
+
+var src = new IloOplModelSource("/home/homero/localgoogledrive/MIRI-DS/AMMM-Project/ILP/model01.mod");
+
+var def = new IloOplModelDefinition(src);
+
+var cplex = new IloCplex();
+
+var logname = "../../Results/Pending/test-oplrun-from-python.json";
+var ofile = new IloOplOutputFile(logname, true);
+ofile.writeln("[");
+ofile.close();
+
+var tilim = 300.0;
+
+
+
+
+
+
+myTest(def, cplex,"./test-oplrun-from-python.dat", logname, 1800.0,  "SUCCESS", 1 );
+var ofile = new IloOplOutputFile(logname, true);
+ofile.writeln("{\"end\":\"end\"}]");
+ofile.close();
+
+def.end();
+cplex.end();
+src.end(); 
+ 
+};
